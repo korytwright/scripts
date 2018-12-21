c981f8bd86e34795be675fe77d4307a1fe399a74
korytwright <45668526+korytwright@users.noreply.github.com>
2018-12-21 09:23:33 -0600

    Create fixed_test.sh

---
+++ fixed_test.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+it [[ -f ips.txt ]]; then
+      rm ips.txt
+      echo "this file alreadt existed, a new one was made in its place"
+fi
+
+ping -c 4 google.com > ips.txt
+
+#get 4th field from all lines
+#copy file in order to extract just the IP
+awk '{print $4}' ips.txt > copy.txt
+
+awk 'NR==2 {print;exit}' copy.txt > copy1.txt
+
+cat copy1.txt | cut -d ":" -f 1 > ready.txt
+
+if [[ -f ready.txt ]]; then
+      VAR1=(cat readt.txt)
+fi
